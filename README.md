d4d68bc59db5d955d11b47ed89a9264856526c01
ClusterWhisperer <clusterwhisperer@gmail.com>
2017-05-22 16:25:24 -0700

    Setup .. first commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+### eNoseLab Project 
+
+Motivated by the [paper](https://www.researchgate.net/publication/305385157_Online_decorrelation_of_humidity_and_temperature_in_chemical_sensors_for_continuous_monitoring) presented by 
+Ramon Huerta, Thiago Mosqueiro, Jordi Fonollosa, Nikolai Rulkov, Irene Rodriguez-Lujan. Online Decorrelation of Humidity and Temperature in Chemical Sensors for Continuous Monitoring. Chemometrics and Intelligent Laboratory Systems 2016.
+
+This project aims to build a streaming infrastructure to process the IoT data in real time, during this process we wanted to 
+explore different technologies (ex: Spark Streaming, Kafka Streaming, Apache flink etc) that a better fit for this usecase. 
+
+@todo ... add more details.
+
+
+
+## Reference:
+- [UCI public dataset](https://archive.ics.uci.edu/ml/datasets/Gas+sensors+for+home+activity+monitoring)
+- [GitHub Code to process UCI dataset](https://github.com/thmosqueiro/ENose-Decorr_Humdt_Temp)
---
+++ agent/simulated_mox_gateway.py
@@ -0,0 +1,134 @@
+#! /usr/bin/python
+import json
+import datetime
+from optparse import OptionParser
+
+'''
+simulated_mox_gateway.py -  reads the HT_Sensor_dataset.dat file and HT_Sensor_metadata.dat file 
+and builds json.
+
+
+Ex:
+id time	      R1         R2         R3         R4         R5         R6         R7        R8        Temp.      Humidity
+0  -0.999750  12.862100  10.368300  10.438300  11.669900  13.493100  13.342300  8.041690  8.739010  26.225700  59.052800
+
+simulator would read the static file (@param -f) and converts each row to json object of following format:
+
+{
+  "guid": "0-ZZZ123456787B",
+  "eventTime": "2017-05-18T23:37:32.618488Z",
+  "payload": {
+     "format": "urn:enose:gateway:summary",
+     "data": {
+       "R1": 12.862100,
+       "R2": 10.368300,
+       "R3": 10.438300,
+       "R4": 11.669900,
+       "R5": 13.342300,
+       "R6": 13.342300,
+       "R7": 8.041690,
+       "R8": 8.739010,
+       "temp": 26.225700,
+       "humid": 59.052800
+     }
+   }
+}
+
+
+Usage:
+./simulated_mox_sensor.py -i 38412f32-e708-447c-8842-6d65357aac89 -f ../data/HT_Sensor_dataset.dat
+
+
+'''
+
+
+# metadata inmem lookup (only 100 inductions)
+metadata_lookup = {}
+
+
+# metadata file column offset
+METADATA_ID = 0
+METADATA_DATE = 1
+METADATA_CLASS = 2
+METADATA_INDUCTION_START = 3
+METADATA_INDUCTION_DURATION = 4
+
+
+# data file column offset
+DATA_OFFSET_ID = 0 
+DATA_OFFSET_TIME = 1
+DATA_OFFSET_R1 = 2
+DATA_OFFSET_R2 = 3
+DATA_OFFSET_R3 = 4
+DATA_OFFSET_R4 = 5
+DATA_OFFSET_R5 = 6
+DATA_OFFSET_R6 = 7
+DATA_OFFSET_R7 = 8
+DATA_OFFSET_R8 = 9 
+DATA_OFFSET_TEMP = 10
+DATA_OFFSET_HUMID = 11
+
+def load_metadata(file):
+	with open(file) as f:
+		next(f)
+		for line in f:
+			words = line.split()
+			metadata_lookup[words[0]] = words
+
+def get_date_iso_format(ind_id, event_offset):
+	induction_metadata=metadata_lookup.get(ind_id)
+	induction_date = induction_metadata[METADATA_DATE]
+	induction_time=float(event_offset)*60 # convert to mins. 
+	return (datetime.datetime.strptime(induction_date,'%m-%d-%y') + datetime.timedelta(minutes = induction_time)).isoformat()
+
+
+def build_json_from_data_line(line, guid):
+	words = line.split()
+	l = {}
+	l['guid'] = guid
+	l['eventTime'] = get_date_iso_format(words[DATA_OFFSET_ID], words[DATA_OFFSET_TIME])
+	p = {}
+	p['format'] = 'urn:enose:gateway:summary'
+	d = {}
+	d['R1'] = float(words[DATA_OFFSET_R1])
+	d['R2'] = float(words[DATA_OFFSET_R2])
+	d['R3'] = float(words[DATA_OFFSET_R3])
+	d['R4'] = float(words[DATA_OFFSET_R4])
+	d['R5'] = float(words[DATA_OFFSET_R5])
+	d['R6'] = float(words[DATA_OFFSET_R6])
+	d['R7'] = float(words[DATA_OFFSET_R7])
+	d['R8'] = float(words[DATA_OFFSET_R8])
+	d['temp'] = float(words[DATA_OFFSET_TEMP])
+	d['humid'] = float(words[DATA_OFFSET_HUMID])
+	p["data"] = d
+	l["payload"] = p
+	return json.dumps(l)
+
+def main():
+	parser = OptionParser()
+	parser.add_option("-d", "--data_file", dest="data_file", help="input data file")
+	parser.add_option("-m", "--meta_data_file", dest="metadata_file", help="input metadata file")
+	parser.add_option("-i", "--gateway_id", dest="guid", help="GUID")
+	(options, args) = parser.parse_args()
+
+	json_data=[]
+
+	load_metadata(options.metadata_file)
+
+	with open(options.data_file) as f:
+		next(f) #skip the header line
+		for line in f:
+			json_data.append(build_json_from_data_line(line, options.guid))
+
+			
+	print json_data[-100:]		
+
+	#print options
+	#print args
+	# if len(options) != 2:
+	# 	parser.error("wrong number of arguments")
+
+	
+
+if __name__ == '__main__':
+	main()
